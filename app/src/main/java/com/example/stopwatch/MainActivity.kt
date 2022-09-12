0a5578447e94aaed17dfa756435fdde4b05d4caf
David Chong <davidchong1224@gmail.com>
2022-09-12 09:19:40 -0700

    finish

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -17,4 +17,31 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         Log.d(TAG, "onStart: ")
     }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: restarted")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: resumed")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: paused")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: stopped")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: destroyed")
+    }
+
+
 }
