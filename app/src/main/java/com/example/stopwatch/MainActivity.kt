c3e33bca2a139c895939848d5143030afacea27b
David Chong <davidchong1224@gmail.com>
2022-09-20 08:33:01 -0700

    finish

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,16 +1,56 @@
 package com.example.stopwatch
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.SystemClock
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Chronometer
+import java.lang.Boolean.FALSE
+import java.lang.Boolean.TRUE
 
 class MainActivity : AppCompatActivity() {
+    lateinit var start : Button
+    lateinit var reset : Button
+    lateinit var stopwatch : Chronometer
     companion object {
         val TAG = "MainActivity"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        wireWidgets()
+        var isworking = FALSE
+        var time = 0.toLong()
+
+        start.setOnClickListener {
+            if(!isworking){
+                stopwatch.start()
+                isworking = TRUE
+                start.setText("stop")
+                stopwatch.base = SystemClock.elapsedRealtime() - time
+
+
+            } else {
+                stopwatch.stop()
+                isworking= FALSE
+                start.setText("start")
+                time = SystemClock.elapsedRealtime() - stopwatch.base
+
+            }
+
+
+        }
+        reset.setOnClickListener {
+            stopwatch.setBase(SystemClock.elapsedRealtime());
+            time=0
+            stopwatch.stop();
+        }
+
+
     }
 
     override fun onStart() {
@@ -42,6 +82,10 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         Log.d(TAG, "onDestroy: destroyed")
     }
-
+    private fun wireWidgets(){
+        start = findViewById(R.id.button_main_start)
+        reset = findViewById(R.id.Reset)
+        stopwatch = findViewById(R.id.chronometer_main_stopwatch)
+    }
 
 }
